d63a134ee459f74de644c74a8c6a4d7442d8476e
Priyank Juyal <juyalpriyank@gmail.com>
2019-12-08 01:54:36 +0530

    Added README.md before making the repo public

---
+++ README.md
@@ -0,0 +1,51 @@
+# WhatsApp-Scraping
+Python script to scrape all the chats from WhatsApp Web.
+
+#Description
+Asynchronous WhatsApp Scraper written in Python. I have tried to keep this script independent of the changing classnames of WhatsApp web. Simply prints the text from each chat to the console. 
+
+# Requirements 
+Script uses Python3.5+.
+All the libraries that we are going to use are in the [requirements.txt](requirements.txt) file.
+You can install it with PIP in the terminal with:
+```
+pip install -r requirements.txt
+```
+
+## Selenium
+Selenium requires a driver to interface with the chosen browser. This script uses the Chrome driver. Please Download your driver from below link. NOTE:- The driver version should match your Chrome browser's version.
+
+* [Chrome](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+
+>For more information you can go to the [Selenium Website](http://selenium-python.readthedocs.io/installation.html) at the installation section.
+
+#Limitations
+*  Doesn't prints the link of the pictures/videos.
+*  Doesn't scrape the archived chats.
+*  Marked replies to texts are not seperated as of now.
+
+# Settings
+**AT THIS MOMENT THE PROJECT ONLY WORKS WITH CHROME**
+**THE SCRIPT IS RUN USING AN IDE LIKE IPYTHON**
+**BELOW ARE THE STEP-BY-STEP GUIDANCE FOR THE SAME**
+
+
+# Step-1:
+*  Clone the repo
+*  Open IPython and import the script.py(using the below command) which will launch the browser and redirect you to whatsapp web.
+``` 
+import script
+```
+
+# Step-2:
+*  Scan the QR code and wait for the page to load.
+*  Now import the start_scrape() function from script.py
+``` 
+from script import start_scrape
+```
+
+# Step-3:
+*  Now call the function and you will see the script printing "processing..." until it completely scrolls the chat window. When the chat ends it will print all the text from the chat to console and move on to the next chat. 
+ ```
+await start_scrape()
+ ``` 
--- script.py
+++ script.py
@@ -4,16 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-already_scraped = {}
+already_scraped = {}            #A hash to keep track of all the chats that have been already scraped by the script
 
-
-def launch_browser():
-    """ later make just one function and use sleep so that browser will not keep shutting down"""
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    #options.add_argument('--incognito')
-    driver = webdriver.Chrome("chromedriver", options=options)
-    driver.get('https://web.whatsapp.com')
+#launches browser
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+#options.add_argument('--incognito')
+driver = webdriver.Chrome("--driver_path", options=options)         #Give the path of your chrome driver
+driver.get('https://web.whatsapp.com')
 
 async def start_scrape(scroll=0):
     """ This is a test function which will contain the possible scraping ways"""
@@ -21,20 +19,19 @@ async def start_scrape(scroll=0):
     driver.execute_script(f"arguments[0].scrollTop = {scroll}", target)
     source = driver.page_source
     soup = BeautifulSoup(source, 'lxml')
-    a = soup.findAll("div", {"id": "pane-side"})[0] #This will fetch all the left side panel's code
-    soup = BeautifulSoup(str(a), 'lxml')
-    _list = soup.findAll('div', {'tabindex' : '-1'})[1:] #This _list will contain all the chats div , 0th index is ignored([1:]) because it will be parent div with all the children div in it
-    for el in _list:
-        text = el.find('span', {'title': True})['title']
-        if text not in already_scraped.keys():
-            driver.find_element_by_xpath(f"//span[text()='{text}']").click()
+    left_panel = soup.findAll("div", {"id": "pane-side"})[0] #This will fetch all the left side panel's code
+    left_panel_soup = BeautifulSoup(str(left_panel), 'lxml')
+    chat_div_list = left_panel_soup.findAll('div', {'tabindex' : '-1'})[1:] #This _list will contain all the chats div , 0th index is ignored([1:]) because it will be parent div with all the children div in it
+    for chat_div in chat_div_list:
+        chat_name = chat_div.find('span', {'title': True})['title']
+        if chat_name not in already_scraped.keys():
+            driver.find_element_by_xpath(f"//span[text()='{chat_name}']").click()
             element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "main")))
-            already_scraped.update({text: True})
-            c = await reload_soup(driver)
-            while await print_to_console(driver, c):
-                print('pass')
-                c = await reload_soup(driver)
-        break
+            already_scraped.update({chat_name: True})
+            reloaded_soup = await reload_soup(driver)
+            while await print_to_console(driver, reloaded_soup):
+                print('processing...')
+                reloaded_soup = await reload_soup(driver)
     scroll += 350
     start_scrape(scroll)
 
@@ -45,44 +42,24 @@ async def print_to_console(driver, c):
         return True
 
     except Exception as e:
-        c = await reload_soup(driver)
+        reloaded_soup = await reload_soup(driver)
         print(e)
-        for l in c:
-            print(l.text)
+        for text_div in reloaded_soup:
+            print(text_div.text)
+        print('\n \n \n Scrapping next chat.......')
         return False
 
 
 async def reload_soup(driver):
     source = driver.page_source
     soup = BeautifulSoup(source, 'lxml')
-    a = soup.find('div', {"id" : "main"})
-    soup = BeautifulSoup(str(a), 'lxml')
-    b = soup.find('div', {"class" : "copyable-area"})
-    b = list(b)
-    b= b[2]
-    soup = BeautifulSoup(str(b), 'lxml')
-    c = soup.findAll('div',{"class" : "copyable-text"})     #This has all divs for each message which has information if the message is replied to or normal message
-    return c
-
-"""
-code for chat window
-source = driver.page_source
-soup = BeautifulSoup(source, 'lxml')
-a = soup.find('div', {"id" : "main"})
-soup = BeautifulSoup(str(a), 'lxml')
-b = soup.find('div', {"class" : "copyable-area"})
-b = list(b)
-b= b[2]
-soup = BeautifulSoup(str(b), 'lxml')
-c = soup.findAll('div',{"class" : "copyable-text"})     #This has all divs for each message which has information if the message is replied to or normal message
-
-for l in c:
-    print(l.text)
-
-driver.find_element_by_xpath("//div[contains(@title,'load earlier messages…')]").location_once_scrolled_into_view                      #Scrolls/ calls the loader of the chat window for new messages
-
-"""
-
+    all_soup = soup.find('div', {"id" : "main"})
+    soup = BeautifulSoup(str(all_soup), 'lxml')
+    filtered_soup = soup.find('div', {"class" : "copyable-area"})
+    filtered_soup = list(filtered_soup)[2]
+    soup = BeautifulSoup(str(filtered_soup), 'lxml')
+    final_soup = soup.findAll('div',{"class" : "copyable-text"})     #This has all divs for each message which has information if the message is replied to or normal message
+    return final_soup
 
 
 if __name__ == '__main__':
